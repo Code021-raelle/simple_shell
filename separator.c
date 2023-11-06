3cd280373d7704b6b077e331fe7e13651efc9338
Gabriel Akinshola <gabrielakinshola3@gmail.com>
2023-11-06 15:45:29 +0100

    separator.c

---
+++ separator.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+/**
+ * separator - Separates command recieved from stdin by ;
+ *
+ * @input: string gathered from stdin
+ *
+ * Return: parsed strings to be used as commands
+ */
+
+char **separator(char *input)
+{
+	char **commands;
+	char *command;
+	int i;
+	int buffsize = BUFFSIZE;
+
+	if (input[0] == ' ' && input[_strlen(input)] == ' ')
+		exit(0);
+	if (input == NULL)
+		return (NULL);
+	commands = malloc(sizeof(char *) * buffsize);
+	if (!commands)
+	{
+		free(commands);
+		perror("hsh");
+		return (NULL);
+	}
+
+	command = _strtok(input, ";&");
+	for (i = 0; command; i++)
+	{
+		commands[i] = command;
+		command = _strtok(NULL, ";&");
+	}
+	commands[i] = NULL;
+
+	return (commands);
+}
+
